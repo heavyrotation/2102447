22f0117b86c33117734f6d88646315118edea120
heavyrotation <38372316+heavyrotation@users.noreply.github.com>
2019-10-07 13:42:15 +0700

    Add files via upload

---
+++ M5StackFire_WiFiWPA2Personal/M5StackFire_WiFiWPA2Personal.ino
@@ -0,0 +1,32 @@
+#include <M5Stack.h>
+#include <WiFi.h>
+
+#define WIFI_SSID "T's iPhone" 
+#define WIFI_PASS "poporing" 
+
+WiFiClient client;
+char strBuffer[100];
+
+void setup() {
+
+  M5.begin();
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.println("wifi connect begin");
+  M5.update();
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    M5.Lcd.println("...");
+    delay(1000);
+  }
+  M5.Lcd.println("wifi connected");
+  IPAddress ip = WiFi.localIP();
+  M5.Lcd.println("IP  Address: " + String(WiFi.localIP().toString()));
+  M5.update();
+
+}
+
+void loop() {
+  M5.update();
+  delay(200);
+}
